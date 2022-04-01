387db8e1de791d79f73b972f9a48dce6bc321ffa
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2022-04-01 15:05:22 +0200

    added CRUD

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { Construct } from "constructs";
 import { App, AssetType, TerraformAsset, TerraformOutput, TerraformStack } from "cdktf";
-import {AwsProvider, s3, lambdafunction, iam, apigatewayv2} from "./.gen/providers/aws";
+import {AwsProvider, s3, lambdafunction, iam, apigateway, dynamodb} from "./.gen/providers/aws";
 import path = require("path");
 
 class MyStack extends TerraformStack {
@@ -9,6 +9,34 @@ class MyStack extends TerraformStack {
 
     new AwsProvider(this, "aws", {
       region: "eu-west-1"
+    });
+
+    const asset = new TerraformAsset(this, "lambda-asset", {
+      path: path.resolve(__dirname, "./src"),
+      type: AssetType.ARCHIVE, // if left empty it infers directory and file
+    });
+
+    const assetBucket = new s3.S3Bucket(this, "bucket", {
+      bucket: 'lambda-asset-bucket-test1231234',
+    });
+
+    const lambdaArchive = new s3.S3BucketObject(this, "lambda-zip", {
+      bucket: assetBucket.bucket,
+      key: asset.fileName,
+      source: asset.path,
+      sourceHash: asset.assetHash
+    });
+
+    const db = new dynamodb.DynamodbTable(this, "table", {
+      name: "table-test1231234",
+      billingMode: "PAY_PER_REQUEST",
+      hashKey: "id",
+      attribute:[
+        {
+          name: "id",
+          type: "S"
+        }
+      ]
     });
 
     const lambdaRole = {
@@ -24,24 +52,8 @@ class MyStack extends TerraformStack {
         }
       ]
     };
-
-    const asset = new TerraformAsset(this, "lambda-asset", {
-      path: path.resolve(__dirname, "./src"),
-      type: AssetType.ARCHIVE, // if left empty it infers directory and file
-    });
-
-    const assetBucket = new s3.S3Bucket(this, "bucket", {
-      bucket: 'lambda-asset-bucket-pedram123123',
-    });
-
-    const lambdaArchive = new s3.S3BucketObject(this, "lambda-zip", {
-      bucket: assetBucket.bucket,
-      key: asset.fileName,
-      source: asset.path,
-    });
-
     const role = new iam.IamRole(this, "lambda-execution-role", {
-      name: 'lambda-execution-role-pedram123123',
+      name: 'lambda-execution-role-test1231234',
       assumeRolePolicy: JSON.stringify(lambdaRole)
     });
 
@@ -50,30 +62,163 @@ class MyStack extends TerraformStack {
       role: role.name
     });
 
+    new iam.IamRolePolicyAttachment(this, "rolePolicyDB", {
+      policyArn: "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+      role: role.name
+    });
+
+
     const lambdaFunc = new lambdafunction.LambdaFunction(this, "lambdaFunc", {
       functionName: 'HelloLambda',
       s3Bucket: assetBucket.bucket,
       s3Key: lambdaArchive.key,
+      sourceCodeHash: lambdaArchive.sourceHash,
       handler: 'index.handler',
       runtime: 'nodejs14.x',
-      role: role.arn
+      role: role.arn,
+      environment: {
+        variables: {
+          "TABLE_NAME": db.name,
+          "PRIMARY_KEY": "id"
+        }
+      }
     });
 
-    const api = new apigatewayv2.Apigatewayv2Api(this, "apig", {
-      name: name,
-      protocolType: "HTTP",
-      target: lambdaFunc.arn
+    const api = new apigateway.ApiGatewayRestApi(this, "api", {
+      name: "api-test1231234",
+      description: "api-test1231234"
+    });
+    
+    const resource = new apigateway.ApiGatewayResource(this, "resource", {
+      restApiId: api.id,
+      parentId: api.rootResourceId,
+      pathPart: "resource"
     });
 
-    new lambdafunction.LambdaPermission(this, "apig-lambda", {
+    const postApi = new apigateway.ApiGatewayMethod(this, "postApi", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: "POST",
+      authorization: "NONE"
+    });
+
+    const getApi = new apigateway.ApiGatewayMethod(this, "getApi", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: "GET",
+      authorization: "NONE"
+    });
+
+    const putApi = new apigateway.ApiGatewayMethod(this, "putApi", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: "PUT",
+      authorization: "NONE"
+    });
+
+    const delApi = new apigateway.ApiGatewayMethod(this, "delApi", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: "DELETE",
+      authorization: "NONE"
+    });
+
+
+    new apigateway.ApiGatewayIntegration(this, "integration1", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: getApi.httpMethod,
+      integrationHttpMethod: "POST",
+      type: "AWS_PROXY",
+      uri: lambdaFunc.invokeArn,
+    });
+
+
+    new apigateway.ApiGatewayIntegration(this, "integration2", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: postApi.httpMethod,
+      integrationHttpMethod: "POST",
+      type: "AWS_PROXY",
+      uri: lambdaFunc.invokeArn,
+    });
+
+
+
+    new apigateway.ApiGatewayIntegration(this, "integration3", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: delApi.httpMethod,
+      integrationHttpMethod: "POST",
+      type: "AWS_PROXY",
+      uri: lambdaFunc.invokeArn,
+    });
+
+
+
+    new apigateway.ApiGatewayIntegration(this, "integration4", {
+      restApiId: api.id,
+      resourceId: resource.id,
+      httpMethod: putApi.httpMethod,
+      integrationHttpMethod: "POST",
+      type: "AWS_PROXY",
+      uri: lambdaFunc.invokeArn,
+    });
+
+
+    const lambPermission=new lambdafunction.LambdaPermission(this, "apig-lambda", {
       functionName: lambdaFunc.functionName,
+      statementId: "AllowExecutionFromApiGateway",
       action: "lambda:InvokeFunction",
       principal: "apigateway.amazonaws.com",
       sourceArn: `${api.executionArn}/*/*`,
+      dependsOn:[api]
     });
 
-    new TerraformOutput(this, 'output', {
-      value: api.apiEndpoint
+    const apiDep = new apigateway.ApiGatewayDeployment(this, "api-deployment", {
+      restApiId: api.id,
+      dependsOn: [lambPermission]
+    });
+
+    const apiStage = new apigateway.ApiGatewayStage(this, "api-stage", {
+      restApiId: api.id,
+      stageName: "prod",
+      deploymentId: apiDep.id,
+      dependsOn: [apiDep]
+    });
+
+    const apiKey = new apigateway.ApiGatewayApiKey(this, "api-key", {
+      name: "api-key-test1231234",
+      description: "api-key-test1231234",
+      enabled: true
+    });
+
+    const usage = new apigateway.ApiGatewayUsagePlan(this, "usage-plan", {
+      name: "usage-plan-test1231234",
+      description: "usage-plan-test1231234",
+      apiStages: [
+        {
+          apiId: api.id,
+          stage: apiStage.stageName
+        }
+      ],
+      throttleSettings: {
+        burstLimit: 10,
+        rateLimit: 10
+      },
+      dependsOn: [apiKey]
+    });
+
+    new apigateway.ApiGatewayUsagePlanKey(this, "usage-key", {
+      keyId: apiKey.id,
+      keyType:"API_KEY",
+      usagePlanId: usage.id,
+      dependsOn: [usage]
+    });
+
+    new TerraformOutput(this, "output", {
+      value: apiStage.invokeUrl,
+      description: "API URL"
     });
 
   }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,86 @@
 
-export const handler = async () : Promise <any> => {
-    return { statusCode: 200, body: 'Hello, World!' };
-  
-};
+import * as AWS from 'aws-sdk';
+import {v4 as uuidv4} from 'uuid';
+
+const db = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = process.env.TABLE_NAME || '';
+const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
+
+
+export const handler = async (event: any = {}): Promise<any> => {
+    //post request
+    if (event.httpMethod === 'POST') {
+        const body = JSON.parse(event.body);
+        const id = uuidv4();
+        const params = {
+            TableName: TABLE_NAME,
+            Item: {
+                [PRIMARY_KEY]: id,
+                ...body
+            }
+        };
+        await db.put(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                id
+            })
+        };
+    }
+    else if (event.httpMethod === 'GET') {
+        const params = {
+            TableName: TABLE_NAME
+        };
+        const data = await db.scan(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Items)
+        };
+    }
+
+    //delete request
+    else if (event.httpMethod === 'DELETE') {
+        const id = event.pathParameters.id;
+        const params = {
+            TableName: TABLE_NAME,
+            Key: {
+                [PRIMARY_KEY]: id
+            }
+        };
+        await db.delete(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                id
+            })
+        };
+    }
+
+    //put request
+    else if (event.httpMethod === 'PUT') {
+        const id = event.pathParameters.id;
+        const body = JSON.parse(event.body);
+        const params = {
+            TableName: TABLE_NAME,
+            Key: {
+                [PRIMARY_KEY]: id
+            },
+            UpdateExpression: 'set #name = :name',
+            ExpressionAttributeNames: {
+                '#name': 'name'
+            },
+            ExpressionAttributeValues: {
+                ':name': body.name
+            },
+            ReturnValues: 'UPDATED_NEW'
+        };
+        await db.update(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                id
+            })
+        };
+    }
+
+}
